40adc9e0a4e3f6f97a5afa24398c301334fa7c1b
M S Devanarayan <116883606+msdnarayan@users.noreply.github.com>
2024-01-13 00:16:30 +0530

    Update Dashboard.jsx

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,34 +1,47 @@
 import React from 'react'
 import { BrowserRouter as Router, Route,Routes, Link, Redirect, Navigate} from 'react-router-dom';
+import image1 from './imgs/allstudents.png';
+import image2 from './imgs/student.png';
+import image3 from './imgs/attendance.png';
+import image4 from './imgs/department.png';
+import image5 from './imgs/search.png';
+import image6 from './imgs/record.png';
 import './Dashboard.css';
 
 function Dashboard() {
+  const imagePaths = [image1,image2,image3,image4,image5,image6];
   return (
-    <div>
-      <nav>
-        <ul>
-        <li>
+    <div className='dashboard'>
+        <div>
+          <div className='dashboard-item'> 
+          <img src={imagePaths[0]} alt='' /><br/> <br/>   
             <Link to="/studdetails">All Students</Link>
-          </li>
-          <li>
+          </div>
+          <div className='dashboard-item'>  
+          <img src={imagePaths[1]} alt='' /> <br/><br/>
             <Link to="/addstud">Add a Student</Link>
-          </li>
-          <li>
+          </div>
+          </div>
+          <div>
+          <div className='dashboard-item'>
+          <img src={imagePaths[2]} alt='' /> <br/><br/>
             <Link to="/addattend">Add Attendance</Link>
-          </li>
-          <li>
+          </div>
+          <div className='dashboard-item'>
+          <img src={imagePaths[3]} alt='' /><br/><br/> 
             <Link to="/adddepart">Add Department</Link>
-          </li>
-          <li>
+          </div>
+          </div>
+          <div>
+          <div className='dashboard-item'>
+          <img src={imagePaths[4]} alt='' /> <br/><br/>
             <Link to="/search">Search a Student</Link>
-          </li>
-          <li>
+          </div>
+          <div className='dashboard-item'>
+          <img src={imagePaths[5]} alt='' /><br/><br/>
             <Link to="/attendlist">Attendance Record</Link>
-          </li>
-
-
-        </ul>
-      </nav>
+          </div>
+          </div>
     </div>
   )
 }
